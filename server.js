1e12e0380fc23887c38414f3e8f0baa00f89a5b8
funschoolwork <evantravostino@myyahoo.com>
2026-02-25 21:06:43 -0500

    Update MOTD with new message and credits

--- server.js
+++ server.js
@@ -50,8 +50,11 @@ function initData() {
 
       // ── Display ──────────────────────────────────────────────────────
       // Placeholders: {0}=version, {1}=mod count, {2}=build type, {3}=build timestamp
-      "motd": "You are using build {0}. Welcome to ii's Stupid Menu! SERVER FIX BY N5!!!",
-      "discord-invite": "https://discord.gg/iidk",
+      "motd": "You are using build {0}. This menu was created by iiDk (the pdf file himself) fixed by N5 " +
+        "This menu is completely free and open sourced, if you paid for this menu you have been scammed. " +
+        "There are a total of <b>{1}</b> mods on this menu. " +
+        "<color=red>I, iiDk, am not responsible for any bans using this menu.</color> " +
+        "If you get banned while using this, it's your responsibility.\n\nCurrent menu status: <b>CONNECTED!</b>\nMade with <3 by iiDk, kingofnetflix, and N5\n\n<alpha=128>{2} {0} {3}<alpha=255>",
 
       // ── Admins ───────────────────────────────────────────────────────
       // TO ADD AN ADMIN: add an object to this array:
